49f9293b3ddc1f948d4b4e7a81f84aaa64a8ec59
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-03-16 16:47:53 +0500

    Register Form

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/auth/register-form"
+
+const SignupPage = () => {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  )
+}
+
+export default SignupPage
